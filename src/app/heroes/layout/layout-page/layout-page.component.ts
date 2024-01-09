d135a0009e655cf1224e9fab66668be4834fffa8
Abel Borit <abelboritguitton@gmail.com>
2024-01-09 14:34:30 -0500

    [FEAT] layout-page.component & layout-page.component styles

--- src/app/heroes/layout/layout-page/layout-page.component.ts
+++ src/app/heroes/layout/layout-page/layout-page.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-layout-page',
   templateUrl: './layout-page.component.html',
-  styleUrls: ['./layout-page.component.css']
+  styleUrls: ['./layout-page.component.css'],
 })
 export class LayoutPageComponent {
-
+  public sidebarItems = [
+    /* al hacerlo así como ./list va a navegar a la url list pero desde el path donde se encuentra, es decir, si me encuentro en heroes entonces sería heroes/list */
+    { label: 'List', icon: 'label', url: './list' },
+    { label: 'Add New', icon: 'add', url: './new-hero' },
+    { label: 'Search', icon: 'search', url: './search' },
+  ];
 }
