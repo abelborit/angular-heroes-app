7676468756858a38a346c3e05bac5346947a841d
Abel Borit <abelboritguitton@gmail.com>
2024-01-12 12:27:35 -0500

    [FEAT] handleGetHeroById & hero-page.component & ngOnInit

--- src/app/heroes/pages/hero-page/hero-page.component.ts
+++ src/app/heroes/pages/hero-page/hero-page.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
+import { Hero } from '../../interfaces/hero.interface';
+import { ActivatedRoute, Router } from '@angular/router';
+import { delay, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-hero-page',
   templateUrl: './hero-page.component.html',
-  styleUrls: ['./hero-page.component.css']
+  styleUrls: ['./hero-page.component.css'],
 })
-export class HeroPageComponent {
+export class HeroPageComponent implements OnInit {
+  /* para leer la url se usará el servicio propio en el router que es ActivatedRoute */
+  constructor(
+    private heroesService: HeroesService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) {}
 
+  /* se coloca como opcional porque cuando el componente se monta en un determinado punto en el tiempo no tenemos ningún valor y es null en ese momento */
+  public heroInfo?: Hero;
+
+  ngOnInit(): void {
+    this.activatedRoute.params
+      .pipe(
+        delay(1500),
+        switchMap(({ id }) => this.heroesService.handleGetHeroById(id))
+      )
+      .subscribe((response) => {
+        // console.log({ response });
+
+        if (!response) return this.router.navigate(['/heroes/list']);
+        this.heroInfo = response;
+        return;
+      });
+  }
+
+  handleGoBack(): void {
+    this.router.navigateByUrl('/heroes/list');
+  }
 }
--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 import { environments } from 'src/environments/environments';
 
@@ -12,5 +12,13 @@ export class HeroesService {
 
   handleGetHeroes(): Observable<Hero[]> {
     return this.httpClient.get<Hero[]>(`${this.baseUrl}/heroes`);
+  }
+
+  /* se regresará un observable que emita algo de tipo Hero o undefined, y se coloca el undefined porque puede ser que el usuario escribió directo en la url un id que no existe y eso me ayudará para hacer alguna validación para salirme de la pantalla e ir a otra, regresar algún error, etc */
+  handleGetHeroById(idHero: string): Observable<Hero | undefined> {
+    /* aquí si el idHero viene undefined entonces nos dará un 404 Not Found y por eso se manejará el error */
+    return this.httpClient
+      .get<Hero>(`${this.baseUrl}/heroes/${idHero}`)
+      .pipe(catchError(() => of(undefined)));
   }
 }
