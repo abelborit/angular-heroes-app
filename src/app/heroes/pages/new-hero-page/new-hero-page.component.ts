c7333324a8d87d0080e7dc63de3666817a7f3f73
Abel Borit <abelboritguitton@gmail.com>
2024-01-17 10:30:24 -0500

    [FEAT] new-hero-page.component

--- src/app/heroes/pages/new-hero-page/new-hero-page.component.ts
+++ src/app/heroes/pages/new-hero-page/new-hero-page.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-new-hero-page',
   templateUrl: './new-hero-page.component.html',
-  styleUrls: ['./new-hero-page.component.css']
+  styleUrls: ['./new-hero-page.component.css'],
 })
 export class NewHeroPageComponent {
-
+  public publishers = [
+    {
+      id: 'DC Comics',
+      value: 'DC - Comics',
+    },
+    {
+      id: 'Marvel Comics',
+      value: 'Marvel - Comics',
+    },
+  ];
 }
