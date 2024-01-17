20da5dde5eb5ee0ecb4db23dc48c552d3d7472b3
Abel Borit <abelboritguitton@gmail.com>
2024-01-17 10:39:07 -0500

    [COMMENTS] new-hero-page.component.ts

--- src/app/heroes/pages/new-hero-page/new-hero-page.component.ts
+++ src/app/heroes/pages/new-hero-page/new-hero-page.component.ts
@@ -17,3 +17,21 @@ export class NewHeroPageComponent {
     },
   ];
 }
+
+/* ******************************************************************************************************************* */
+/*
+Aquí se crea un objeto con los publicadores (publishers), pero en la carpeta de interfaces del módulo se tiene un enum con estos. Se puede hacer uso de este para cargar los valores del select de la siguiente manera:
+
+1. se importa el enum en el archivo new-hero-page.component.ts
+    import { Publisher } from '../../interfaces/hero.interface';
+
+2. se crea el objeto utilizando el tipo
+    public publishers: typeof Publisher = Publisher;
+
+3. en el html el *ngFor debe pasar el objeto por el pipe de 'keyvalues' y mostrar el value
+    <mat-select required>
+         <mat-option *ngFor="let publisher of publishers | keyvalue" [value]="publisher.value">
+            {{ publisher.value }}
+         </mat-option>
+    </mat-select>
+*/
